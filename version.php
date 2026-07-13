f2cff9bff1ca3641d421f5318a581513163ee261
Mikko Haiku <mikko.haiku@iki.fi>
2026-07-13 15:40:21 +0300

    Release

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tiny_fontsize';
-$plugin->release = '0.1.6';
+$plugin->release = '1.0.0';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires = 2022112800;
 $plugin->version = 2026071300;
